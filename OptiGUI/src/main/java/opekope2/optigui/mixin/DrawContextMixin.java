d8c1fe55d5bece9043dd6c8c0274f07886014509
opekope2 <opekope2@gmail.com>
2024-12-10 00:32:28 +0100

    Mix into DrawContext instead of RenderSystem for 1.21.4 compatibility

--- OptiGUI/src/main/java/opekope2/optigui/mixin/DrawContextMixin.java
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/DrawContextMixin.java
@@ -1,16 +1,16 @@
 package opekope2.optigui.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.Identifier;
 import opekope2.optigui.internal.TextureReplacer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(value = RenderSystem.class, /* Inject before Animatica for full compatibility */ priority = 900)
-abstract class RenderSystemMixin {
-    @ModifyVariable(method = "setShaderTexture(ILnet/minecraft/util/Identifier;)V", at = @At("HEAD"), index = 1, argsOnly = true)
-    private static Identifier setShaderTextureMixin(Identifier id) {
+@Mixin(value = DrawContext.class, priority = 900)
+abstract class DrawContextMixin {
+    @ModifyVariable(method = "drawTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIFFIIIIIII)V", at = @At("HEAD"), index = 2, argsOnly = true)
+    private Identifier drawTextureMixin(Identifier id) {
         return TextureReplacer.isReplacingTextures() && id != null ? TextureReplacer.replaceTexture(id) : id;
     }
 }
